c241185c502cbdff08e3cef2dff3850d915007b7
ahoyaclyde <clydensoo@gmail.com>
2025-04-22 07:24:38 +0300

    Project Reinventions : Code Editing

--- Controller.py
+++ Controller.py
@@ -26,7 +26,8 @@ cors = CORS(app , resources = {r"/*" : {"origins" : "*" }})
 
 
 
-
+# Erro Handler Protocol Environs 
+# Auto Redirect To 404.Html 
 @app.errorhandler(404)
 # inbuilt function which takes error as parameter
 def not_found(error):
@@ -35,7 +36,8 @@ def not_found(error):
 
 
 
-
+# Base Route Address For Home/Landing Page Scenarios 
+# Initial Page 
 class Dash_Page_Context(View):
    methods = ['GET']  
    def Render_Year(self):
@@ -52,6 +54,9 @@ class Dash_Page_Context(View):
             return render_template('Home.html' , CompanyID = CompanyID , YEAR = YEAR  )
         
 
+# Subscriber Model Onboarding 
+# Intergrates Clients To Customer Base List  
+# Should Have A Db Impl of Storing  Reg - Subscribers 
 class Onboarding_Platform(View): 
     methods = ['GET', 'POST']  
     def dispatch_request(self) -> str :
@@ -70,6 +75,31 @@ class Onboarding_Platform(View):
 
 
 
+# Contact Us Master Form Submission Protocol
+# Relays Feedback Thru Email After Supply Of Neck Creds
+
+class Contact_Us(View): 
+    methods = ['GET', 'POST']  
+    def dispatch_request(self) -> str :
+        CompanyID = "Osefa Homes & Developers "
+        if request.method == 'POST':
+            EmailAddress = request.form.get("Email")
+            Phone = request.form.get("Contact")
+            Subject = request.form.get("Subject")
+            Subject =Subject + str("Sender Contact -") + Phone  
+          
+            Message = request.form.get("Message")
+         
+            base.Create_Email(EmailAddress , Subject , Message)
+        else: 
+            # Return requested profile thru client connect 
+            return redirect('Home' , CompanyID = CompanyID , Info = Info  )
+
+
+
+# Base Route Address For Contact Page Scenarios 
+# Independent Page Route
+ 
 class Company_Contact_Profile(View):
    methods = ['GET', 'POST']  
    def dispatch_request(self) -> str :
@@ -89,7 +119,7 @@ class Company_Contact_Profile(View):
 app.add_url_rule('/', view_func=Dash_Page_Context.as_view('Home'))
 app.add_url_rule('/contact/portal/', view_func=Company_Contact_Profile.as_view('Contact'))
 app.add_url_rule('/portal/account/create/', view_func=Onboarding_Platform.as_view('join'))
-
+app.add_url_rule('/portal/account/contact-us/', view_func=Contact_Us.as_view('Contact-Osefa'))
 
 
 
